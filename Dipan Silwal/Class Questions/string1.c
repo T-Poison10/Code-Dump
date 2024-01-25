d0454f0faf840c84c8e1d96089fb60dfc39dceb5
T-Poison10 <dipansilwal10@gmail.com>
2024-01-25 10:40:36 +0545

    updated

--- Dipan Silwal/Class Questions/string1.c
+++ Dipan Silwal/Class Questions/string1.c
@@ -4,6 +4,13 @@
 int main()
 {
     char str[dip];
+    int len=0,i=0;
     gets(str);
     puts(str);
+    while (str[i]!='\0')
+    {
+        len++;
+        i++;
+    }
+    printf("%d",len);
 }
